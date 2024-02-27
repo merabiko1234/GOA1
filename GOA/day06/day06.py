61effd24805debe201c2617d588ce064b67d1bbe
merabiko1234 <159827481+merabiko1234@users.noreply.github.com>
2024-02-27 07:52:13 -0800

    Add files via upload

---
+++ GOA/day06/day06.py
@@ -0,0 +1,11 @@
+
+
+first_name = input("What is your first name? ")
+
+last_name = input("What is your last name? ")
+
+age = input("How old are you? ")
+
+
+
+print(f"Your first name is {first_name}, your last name is {last_name}, you are {age} years old,mebo.")
---
+++ GOA/day07/day07.py
@@ -0,0 +1,6 @@
+print("what your age")
+
+print("how many years would you like to travel back in time")
+
+
+
